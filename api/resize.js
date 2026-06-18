6c20fab8102c5b5286bb1e5542a49b32679fcdd1
Sahithi Vankayalapati <sahithi58@gmail.com>
2026-06-17 22:33:23 -0700

    Convert to CommonJS for better Vercel compatibility

--- api/resize.js
+++ api/resize.js
@@ -1,7 +1,7 @@
-import sharp from 'sharp';
-import busboy from 'busboy';
+const sharp = require('sharp');
+const busboy = require('busboy');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
